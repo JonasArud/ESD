e6471e54b764ed20e68893c0e9c7b748a0e08b44
JakeOliverChapman <jocrouge@googlemail.com>
2018-11-05 21:52:48 +0000

    SQL for driver and customer invoice written. Head office SQL implemented. Database updated with new tables.

--- database/tables.sql
+++ database/tables.sql
@@ -44,3 +44,47 @@ CREATE TABLE booking_table (
 INSERT INTO booking_table (driverID, startTime, endTime, customerID, bookingReference, distanceInMiles, paymentAmount, paymentTime, jobCompleted) VALUES('12A3PSHAEMZX5OJA9G60', '9:30:00', '10:00:00', 'AYEV0SEEIBSH6JK4N83S', 'DFRA8LXBPNMMGKQ3HAEL', 9, 17.98, '2018-11-05 09:31:51', 'true');
 INSERT INTO booking_table (driverID, startTime, endTime, customerID, bookingReference, distanceInMiles, paymentAmount, paymentTime, jobCompleted) VALUES('12A3PSHAEMZX5OJA9G60', '10:30:00', '11:00:00', 'AYEV0SEEIBSH6JK4N83S', 'I40DNT3J98419ZQXMMEZ', 9, 17.98, '2018-11-05 10:31:51', 'true');
 INSERT INTO booking_table (driverID, startTime, endTime, customerID, bookingReference, distanceInMiles, paymentAmount, paymentTime, jobCompleted) VALUES('', '11:00:00', '11:30:00', 'AGYE7LENFOLF11A1NXP1', 'TTB2EJ9L0R53VIOFDYZ0', 22, 37.98, '2018-11-05 11:00:21', 'false');
+
+
+CREATE TABLE driver_invoice (
+	invoiceID varchar(20) NOT NULL,
+	driverID varchar(20) NOT NULL,
+	paymentTime timestamp NOT NULL,
+	paymentAmount float(12) DEFAULT 0.00,
+	journeyStart timestamp,
+	journeyEnd timestamp,
+	
+	PRIMARY KEY (invoiceID) 
+) ;
+INSERT INTO driver_invoice (invoiceID, driverID, paymentTime, paymentAmount, journeyStart, journeyEnd) VALUES('a7j3mklf9oqp12khw', '1dw0xm3ill90sn23wpg', '2018-11-05 14:00:00',55.00, '2018-11-05 14:25:00','2018-11-05 15:50:08');
+INSERT INTO driver_invoice (invoiceID, driverID, paymentTime, paymentAmount, journeyStart, journeyEnd) VALUES('b7j3mklf9oqp12khw', '2dw0xm3ill90sn23wpg', '2018-11-05 17:36:20',28.00, '2018-11-05 17:59:30','2018-11-05 18:25:11');
+INSERT INTO driver_invoice (invoiceID, driverID, paymentTime, paymentAmount, journeyStart, journeyEnd) VALUES('c7j3mklf9oqp12khw', '3dw0xm3ill90sn23wpg', '2018-11-05 11:22:05',34.00, '2018-11-05 11:44:12','2018-11-05 12:25:33');
+
+
+CREATE TABLE customer_invoice (
+	invoiceID varchar(20) NOT NULL,
+	customerID varchar(20) NOT NULL,
+	paymentTime timestamp NOT NULL,
+	paymentAmount float(12) DEFAULT 0.00,
+	journeyStart timestamp,
+	journeyEnd timestamp,
+	
+	PRIMARY KEY (invoiceID) 
+) ;
+INSERT INTO customer_invoice (invoiceID, customerID, paymentTime, paymentAmount, journeyStart, journeyEnd) VALUES('a7j3mklf9oqp12khwzn3', '1jw0xm3ill90sn23wpgn', '2018-11-05 14:00:00', 55.00, '2018-11-11 14:25:00','2018-11-05 15:50:08');
+INSERT INTO customer_invoice (invoiceID, customerID, paymentTime, paymentAmount, journeyStart, journeyEnd) VALUES('b7j3mklf9oqp12khwzn3', '2jw0xm3ill90sn23wpgn', '2018-11-05 17:36:20', 28.00, '2018-11-11 17:59:30','2018-11-05 18:25:11');
+INSERT INTO customer_invoice (invoiceID, customerID, paymentTime, paymentAmount, journeyStart, journeyEnd) VALUES('c7j3mklf9oqp12khwzn3', '3jw0xm3ill90sn23wpgn', '2018-11-05 11:22:05', 34.00, '2018-11-11 11:44:12','2018-11-05 12:25:33');
+
+
+CREATE TABLE head_office (
+	adminID varchar(20) NOT NULL,
+	firstName varchar(20) NOT NULL,
+	lastName varchar(20) NOT NULL,
+	email varchar(50) NOT NULL,
+	password varchar(20) NOT NULL,
+	
+	PRIMARY KEY (adminID) 
+) ;
+INSERT INTO head_office (adminID, firstName, lastName, email, password) VALUES('aaj3mklf9oqp12khwzn3', 'Mathias', 'Sibman','gqrekovsky16@yopmail.com', 'adminpass1');
+INSERT INTO head_office (adminID, firstName, lastName, email, password) VALUES('abj3mklf9oqp12khwzn3', 'Benjamin', 'Hubert','ingerk13@yopmail.com', 'adminpass2');
+INSERT INTO head_office (adminID, firstName, lastName, email, password) VALUES('acj3mklf9oqp12khwzn3', 'Ebbe', 'Streme','bjebbe9@yopmail.com', 'adminpass3');
